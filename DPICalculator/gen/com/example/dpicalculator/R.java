3f5f9881873dc76e962a3f96d00ac2b2cef337c9
alekmit <alekmit@gmail.com>
2013-07-09 18:23:28 +0300

    test2

--- DPICalculator/gen/com/example/dpicalculator/R.java
+++ DPICalculator/gen/com/example/dpicalculator/R.java
@@ -27,21 +27,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int editText0=0x7f090002;
-        public static final int spinner0=0x7f090000;
-        public static final int spinner1=0x7f090001;
-        public static final int text0=0x7f090003;
+        public static final int autocompleteX=0x7f080000;
+        public static final int autocompleteY=0x7f080001;
+        public static final int editText0=0x7f080002;
+        public static final int text0=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
-    public static final class menu {
-        public static final int main=0x7f080000;
-    }
     public static final class string {
-        public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int display_size=0x7f060005;
+        public static final int x=0x7f060001;
+        public static final int x_hint=0x7f060002;
+        public static final int y=0x7f060003;
+        public static final int y_hint=0x7f060004;
     }
     public static final class style {
         /** 
--- DPICalculator/src/com/example/dpicalculator/DpiActivity.java
+++ DPICalculator/src/com/example/dpicalculator/DpiActivity.java
@@ -4,102 +4,92 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Menu;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import static java.lang.Math.*;
 
-public class DpiActivity extends Activity implements AdapterView.OnItemSelectedListener, TextWatcher {
+public class DpiActivity extends Activity {
 	
-	private Spinner spinnerX, spinnerY;
+	private AutoCompleteTextView autocompleteX, autocompleteY;
 	private EditText edtDiag;
 	private TextView captText;
-	
-	private int x, y;
-	private double d;
-	private int a, b, dpi;
+
+	private double x, y, d;
+	private int a, b, ppi;
 	
 	private static final double INCH = 25.4;
-	private static final String FORMAT = "density = %d dpi;\nsize = %d x %d mm";
+	private static final String FORMAT = "density = %d ppi\nsize = %d x %d mm";
+	
+	private class TextHandler implements TextWatcher {
+		
+		private View v;
+		
+		TextHandler(View v){
+			this.v = v;
+		}
+
+		@Override
+		public void afterTextChanged(Editable s) {
+			double val = 0;
+			try {
+	        	val = Double.parseDouble(s.toString());
+	        } catch (NumberFormatException  e){
+	        	return;
+	        }
+			if (v == autocompleteX){
+				x = val;
+			} else if (v == autocompleteY){
+				y = val;
+			}
+			else if (v == edtDiag){
+				d = val;
+			} 
+			calculate();
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before, int count) {}
+		
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		spinnerX = (Spinner) findViewById(R.id.spinner0);
-		spinnerY = (Spinner) findViewById(R.id.spinner1);
+		autocompleteX = (AutoCompleteTextView) findViewById(R.id.autocompleteX);
+		autocompleteY = (AutoCompleteTextView) findViewById(R.id.autocompleteY);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-		        R.array.resolutions_set, android.R.layout.simple_spinner_item);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinnerX.setAdapter(adapter);
-		spinnerY.setAdapter(adapter);
-		spinnerX.setOnItemSelectedListener(this);
-		spinnerY.setOnItemSelectedListener(this);
+		        R.array.resolutions_set, android.R.layout.simple_list_item_1);
+		autocompleteX.setAdapter(adapter);
+		autocompleteY.setAdapter(adapter);
+		autocompleteX.addTextChangedListener(new TextHandler(autocompleteX));
+		autocompleteY.addTextChangedListener(new TextHandler(autocompleteY));
 		edtDiag = (EditText) findViewById(R.id.editText0);
-		edtDiag.addTextChangedListener(this);
+		edtDiag.addTextChangedListener(new TextHandler(edtDiag));
 		captText = (TextView) findViewById(R.id.text0);
 	}
-	
-	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        String item = (String) parent.getItemAtPosition(pos);
-        int val = 0;
-        try {
-        	val = Integer.parseInt(item);
-        } catch (NumberFormatException  e){
-        	return;
-        }
-        if (parent == spinnerX) {
-        	x = val;
-        } else if (parent == spinnerY){
-        	y = val;
-        }
-        calculate();
-    }
-
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
 	
 	private void calculate(){
 		if (x == 0 || y == 0 || d == 0){
 			captText.setText(null);
 			return;
 		}
-		double k = sqrt(pow((double) y/x, 2) + 1);
-		double _a = d/k;
+		double k = y/x;
+		double k1 = sqrt(pow(k, 2) + 1);
+		double _a = d/k1;
 		double _b = k * _a;
-		dpi = (int) (x/_a);
+		ppi = (int) (x/_a);
 		a = (int) (_a * INCH);
 		b = (int) (_b * INCH);
-		captText.setText(String.format(FORMAT, dpi, a, b));
+		captText.setText(String.format(FORMAT, ppi, a, b));
 	}
 
-	@Override
-	public void onNothingSelected(AdapterView<?> arg0) {}
-
-	@Override
-	public void afterTextChanged(Editable s) {
-		try {
-			d = Double.parseDouble(s.toString());
-		} catch (NumberFormatException e) {
-			return;
-		}
-		calculate();
-	}
-
-	@Override
-	public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-	@Override
-	public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
 }
